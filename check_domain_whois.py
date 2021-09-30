aca592292e6365324df4a77578e66bc09314f82a
BausPhi <philipp.baus.studium@googlemail.com>
2021-09-30 14:55:49 +0200

    Added status checks

--- check_domain_whois.py
+++ check_domain_whois.py
@@ -16,39 +16,50 @@ def parse_args():
     parser.add_argument("-H", "--Host", type=str, help="Domain to check")
     parser.add_argument("-n", "--nameserver", type=str, help="Expected nameservers")
     parser.add_argument("-r", "--registrar", type=str, help="Expected registrar")
+    parser.add_argument("-s", "--status", type=str, help="Expected status")
     return parser.parse_args()
 
 
 def main():
     args = parse_args().__dict__
     host = args["Host"]
-    expected_ns = args["nameserver"].replace(" ", "").split(",")
+    expected_ns = None
+    if args["nameserver"] is not None:
+        expected_ns = args["nameserver"].replace(" ", "").split(",")
     expected_registrar = args["registrar"]
+    expected_status = None
+    if args["status"] is not None:
+        expected_status = args["status"].replace(" ", "").split(",")
 
     if host is None:
         print("UNKNOWN - No host is given. Use -H <Host>")
         exit(3)
 
-    if expected_ns is None and expected_registrar is None:
-        print("UNKNOWN - Please use -r to give the expected registrar or/and -n to give the expected nameservers")
+    if expected_ns is None and expected_registrar is None and expected_status is None:
+        print("UNKNOWN - Please give at least one expected value with -n, -r or -s")
         exit(3)
 
     w = whois.whois(host)
+
+    # Get whois results
     registrar = None
     if "registrar" in w.keys():
         registrar = w["registrar"]
     ns = None
     if "name_servers" in w.keys():
         ns = w["name_servers"]
+    status = None
+    if "status" in w.keys():
+        status = w["status"]
 
-    if registrar is None and ns is None:
-        print("UNKNOWN - whois did not deliver any nameservers or the registrar for %s" % host)
+    if registrar is None and ns is None and status is None:
+        print("UNKNOWN - whois did not deliver any information to check the expected values for %s" % host)
         exit(3)
 
     # Check Registrars
     if registrar is not None and expected_registrar is not None:
         if registrar.upper() != expected_registrar.upper():
-            print("CRITICAL - Registrar was changed for %s, it doesn't meet the expectations: Expected %s, Actual: %s" % (host, expected_registrar, registrar))
+            print("CRITICAL - Registrar changed for %s, it doesn't meet the expectations: Expected %s, Actual: %s" % (host, expected_registrar, registrar))
             exit(2)
 
     # Check Nameservers
@@ -60,7 +71,26 @@ def main():
             if nameserver.upper() not in ns:
                 wrong_ns.append(nameserver)
     if len(wrong_ns) != 0:
-        print("CRITICAL - Nameservers were changed for %s, they don't meet the expectations: Expected: %s, Actual: %s" % (host, expected_ns, ns))
+        print("CRITICAL - Nameservers changed for %s, they don't meet the expectations: Expected: %s, Actual: %s" % (host, expected_ns, ns))
+        exit(2)
+
+    # Check Status
+    wrong_status = []
+    if status is not None and expected_status is not None:
+        for i in range(len(expected_status)):
+            expected_status[i] = expected_status[i].upper()
+        for j in range(len(status)):
+            status[j] = status[j].upper()
+        for expected in expected_status:
+            found = False
+            for state in status:
+                if expected in state:
+                    found = True
+                    break
+            if not found:
+                wrong_status.append(expected)
+    if len(wrong_status) != 0:
+        print("CRITICAL - Domain status changed for %s, it doesn't contain the expectations: Expected: %s, Actual: %s" % (host, expected_status, status))
         exit(2)
 
     print("OK - The values for %s were like expected" % host)
